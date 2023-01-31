591fc81caf8f1863414a9a3437afe90a2d8128aa
MarselMingaripov <bard1812@gmail.com>
2023-01-31 12:59:57 +0300

    initial commit

---
+++ src/main/java/min/mars/springbootrecipes/SpringBootRecipesApplication.java
@@ -0,0 +1,13 @@
+package min.mars.springbootrecipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRecipesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRecipesApplication.class, args);
+    }
+
+}
